1b760f9c9a92a7c1bcb6fa2c57f10da39c22e991
Mohd Adil <shaikhmdadil191@gmail.com>
2025-12-15 10:44:30 +0530

    created chatbot

--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
-
 from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
+from src.langgraphagenticai.LLMS.groqllm import GroqLLM
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+from src.langgraphagenticai.ui.streamlitui.display_result import DisplayResultStreamlit
 
 def load_langgraph_agenticai_app():
     """
@@ -18,3 +20,33 @@ def load_langgraph_agenticai_app():
         return
     
     user_message = st.chat_input("Enter your message:")
+
+    if user_message:
+        try:
+            #Configure the llm
+            obj_llm_config = GroqLLM(user_controls_input=user_input)
+            model = obj_llm_config.get_llm_model()
+
+            if not model:
+                st.error("LLM model couldnot be initialised")
+                return
+
+            # Initialize and set up the graph based on use case
+            usecase=user_input.get("selected_usecase")
+            if not usecase:
+                st.error("Error: No use case selected.")
+                return
+
+            graph_builder = GraphBuilder(model=model)
+            try:
+                graph = graph_builder.setup_graph(usecase)
+                print(user_message)
+                DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+            except Exception as e:
+                 st.error(f"Error: Graph set up failed- {e}")
+                 return
+
+
+        except Exception as e:
+            st.error(f"Error: Graph set up failed- {e}")
+            return 
